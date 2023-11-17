7ad8532ef0da459ebe1b0a7d3f2f61f04cc1e46c
Sebas <sebastiancocomilo@gmail.com>
2023-11-16 19:39:19 -0500

    Se agrego una nueva funcionalidad al formulario

--- formulario/form.js
+++ formulario/form.js
@@ -1,31 +1,28 @@
-/*const form = document.getElementById("form")
-console.log(form)*/
+const form = document.getElementById("form")
+console.log(form)
+function guardarInformacion(event) {
+    event.preventDefault()
+    const formData = new FormData(event.srcElement)
+    const nombre = formData.get("nombre")
+    const apellido = formData.get("apellido")
+    const correo = formData.get("correo")
+    const contraseña = formData.get("contraseña")
 
-function guardarInformacion() {
-   // event.preventDefault()
-   // const formData = new FormData(event.srcElement)
-
-     nombre= document.getElementById("nombre")
-   // const formData = new FormData()
-  // const nombre = formData.get("nombre")
-  //  const apellido = formData.get("apellido")
- //  const correo = formData.get("correo")
- //   const password = formData.get("password")
-
-    alert();
-
-    /* guardar en localStorage   
+    // guardar en localStorage
     const persona = {
         nombre,
         apellido,
         correo,
-        password
+        contraseña
     }
-    localStorage.setItem(correo, JSON.stringify(persona))*/ 
+    localStorage.setItem(correo, JSON.stringify(persona))
     location.href = "../index.html";
 }
 
-
-/*form.addEventListener("submit", (event) => {
+form.addEventListener("submit", (event) => {
     guardarInformacion(event)
-})*/
+})
+
+
+
+
